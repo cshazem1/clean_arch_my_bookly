32c13f9d5028a631302d2d80fcfd45351315ee03
AQ <cshazemelwazery@gmail.com>
2024-08-14 17:46:38 +0300

    book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  String image;
+  String title;
+  String authorName;
+  num rating;
+  num price;
+
+  BookEntity(this.image, this.title, this.authorName, this.rating, this.price);
+}
