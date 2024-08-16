7cbe74d19a8d4c923acc025ed71534240676dfaa
AQ <cshazemelwazery@gmail.com>
2024-08-16 19:20:19 +0300

    register Adapter

--- lib/constans.dart
+++ lib/constans.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor=Color(0xff100B20);
 const kTransitionDuration=Duration(seconds: 2);
 const kGtSectraFine='Gt Sectra Fine';
+const KFeaturedBox="featuredBox";
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:bookly/core/utils/app_roter.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 import 'constans.dart';
 
-void main() {
+void main() async {
   runApp(const Bookly());
+  Hive.registerAdapter(BookEntityAdapter());
+ await Hive.openBox(KFeaturedBox);
 }
 
 class Bookly extends StatelessWidget {
